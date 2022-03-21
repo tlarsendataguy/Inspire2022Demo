84ea484ecf0e3453a32824b5325683c22280776a
Tom Larsen <larsenthomasj@gmail.com>
2022-03-21 07:33:31 -0400

    use immediate scoping

--- main.js
+++ main.js
@@ -67,27 +67,27 @@ function loadImage() {
 
 function startRender() {
     //get height data from img
-    var data = getHeightData(renderData.img);
+    let data = getHeightData(renderData.img);
 
     // plane
     const scene = new THREE.Scene();
 
-    var sky = new s.Sky();
+    let sky = new s.Sky();
     sky.scale.setScalar(450000)
     scene.add(sky);
 
-    var light = new THREE.DirectionalLight(0xffffff, 1);
+    let light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.y = 40000;
     light.position.z = -40000;
     scene.add(light);
 
-    const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 5000000 );
-    var geometry = new THREE.PlaneGeometry(renderData.sizeFt, renderData.sizeFt , renderData.width-1, renderData.height-1);
-    var material = new THREE.MeshLambertMaterial({color: 0xaaddaa});
-    var plane = new THREE.Mesh( geometry, material );
+    let camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 5000000 );
+    let geometry = new THREE.PlaneGeometry(renderData.sizeFt, renderData.sizeFt , renderData.width-1, renderData.height-1);
+    let material = new THREE.MeshLambertMaterial({color: 0xaaddaa});
+    let plane = new THREE.Mesh( geometry, material );
 
     //set height of vertices
-    for ( var i = 0; i < renderData.pixels(); i++ ) {
+    for ( let i = 0; i < renderData.pixels(); i++ ) {
         plane.geometry.attributes.position.array[i * 3 + 2] = data[i]/10;
     }
 
@@ -118,7 +118,7 @@ function startRender() {
 
     }
 
-    var controls = new c.FlyControls( camera, renderer.domElement );
+    let controls = new c.FlyControls( camera, renderer.domElement );
 
     controls.movementSpeed = 4000;
     controls.domElement = renderer.domElement;
@@ -130,21 +130,21 @@ function startRender() {
 }
 
 function getHeightData(img, ) {
-    var canvas = document.createElement( 'canvas' );
+    let canvas = document.createElement( 'canvas' );
     canvas.width = img.width;
     canvas.height = img.height;
-    var context = canvas.getContext( '2d' );
+    let context = canvas.getContext( '2d' );
 
-    var size = img.width * img.height;
-    var data = new Int32Array( size );
+    let size = img.width * img.height;
+    let data = new Int32Array( size );
 
     context.drawImage(img,0,0);
 
-    var imgd = context.getImageData(0, 0, img.width, img.height);
-    var pix = imgd.data;
+    let imgd = context.getImageData(0, 0, img.width, img.height);
+    let pix = imgd.data;
     const view = new DataView(pix.buffer);
 
-    for (var i = 0; i < size; i++) {
+    for (let i = 0; i < size; i++) {
         view.setUint8(i*4 + 3, 0);
         data[i] = view.getInt32(i*4, true);
     }
