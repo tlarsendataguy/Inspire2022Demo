1e274a7d7947f371b9a2dfd0b3c1b4c51d50e6e6
Tom Larsen <larsenthomasj@gmail.com>
2022-03-21 06:46:40 -0400

    use function generator

--- main.js
+++ main.js
@@ -33,88 +33,87 @@ function getHeightData(img, scale) {
 
 function loadImage(imageUrl, sizeUrl) {
     let img = new Image();
-    img.onload = function() {
-        onLoadImage(img, sizeUrl);
-    }
+    img.onload = generateOnLoadImage(img, sizeUrl);
     img.src = imageUrl;
 }
 
-function onLoadImage(img, sizeUrl) {
+function generateOnLoadImage(img, sizeUrl) {
+    return function() {
+        var sizes;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", sizeUrl, false);
+        xmlhttp.send();
+        if (xmlhttp.status === 200) {
+            sizes = JSON.parse(xmlhttp.responseText);
+        } else {
+            console.error(`error loading size.json`);
+            return;
+        }
 
-    var sizes;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "size.json", false);
-    xmlhttp.send();
-    if (xmlhttp.status === 200) {
-        sizes = JSON.parse(xmlhttp.responseText);
-    } else {
-        console.error(`error loading size.json`);
-        return;
+        //get height data from img
+        var data = getHeightData(img);
+        var feet = sizes.size * 6076.12;
+        var pixels = sizes.width * sizes.height;
+
+        // plane
+        const scene = new THREE.Scene();
+
+        var sky = new s.Sky();
+        sky.scale.setScalar(450000)
+        scene.add(sky);
+
+        var light = new THREE.DirectionalLight(0xffffff, 1);
+        light.position.y = 40000;
+        light.position.z = -40000;
+        scene.add(light);
+
+        const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 5000000 );
+        var geometry = new THREE.PlaneGeometry(feet, feet , sizes.width-1, sizes.height-1);
+        var material = new THREE.MeshLambertMaterial({color: 0xaaddaa});
+        var plane = new THREE.Mesh( geometry, material );
+
+        //set height of vertices
+        for ( var i = 0; i < pixels; i++ ) {
+            plane.geometry.attributes.position.array[i * 3 + 2] = data[i]/10;
+        }
+
+        plane.rotateX(-3.14159/2);
+        geometry.attributes.position.needsUpdate = true;
+        geometry.computeVertexNormals();
+
+        scene.add(plane);
+        camera.position.y = 10000;
+        camera.position.z = feet/2;
+        camera.rotateX(-3.14159/5);
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth-20, window.innerHeight-20 );
+        renderer.toneMapping = THREE.ACESFilmicToneMapping;
+        renderer.toneMappingExposure = 0.7;
+        document.body.appendChild( renderer.domElement );
+
+
+        function animate() {
+            requestAnimationFrame( animate );
+            render();
+        }
+
+        function render() {
+            const delta = clock.getDelta();
+            controls.update( delta );
+            renderer.render( scene, camera );
+
+        }
+
+        var controls = new c.FlyControls( camera, renderer.domElement );
+
+        controls.movementSpeed = 4000;
+        controls.domElement = renderer.domElement;
+        controls.rollSpeed = 1.0;
+        controls.autoForward = false;
+        controls.dragToLook = false;
+
+        animate();
     }
-
-    //get height data from img
-    var data = getHeightData(img);
-    var feet = sizes.size * 6076.12;
-    var pixels = sizes.width * sizes.height;
-
-    // plane
-    const scene = new THREE.Scene();
-
-    var sky = new s.Sky();
-    sky.scale.setScalar(450000)
-    scene.add(sky);
-
-    var light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.y = 40000;
-    light.position.z = -40000;
-    scene.add(light);
-
-    const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 5000000 );
-    var geometry = new THREE.PlaneGeometry(feet, feet , sizes.width-1, sizes.height-1);
-    var material = new THREE.MeshLambertMaterial({color: 0xaaddaa});
-    var plane = new THREE.Mesh( geometry, material );
-
-    //set height of vertices
-    for ( var i = 0; i < pixels; i++ ) {
-        plane.geometry.attributes.position.array[i * 3 + 2] = data[i]/10;
-    }
-
-    plane.rotateX(-3.14159/2);
-    geometry.attributes.position.needsUpdate = true;
-    geometry.computeVertexNormals();
-
-    scene.add(plane);
-    camera.position.y = 10000;
-    camera.position.z = feet/2;
-    camera.rotateX(-3.14159/5);
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth-20, window.innerHeight-20 );
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    renderer.toneMappingExposure = 0.7;
-    document.body.appendChild( renderer.domElement );
-
-
-    function animate() {
-        requestAnimationFrame( animate );
-        render();
-    }
-
-    function render() {
-        const delta = clock.getDelta();
-        controls.update( delta );
-        renderer.render( scene, camera );
-
-    }
-
-    var controls = new c.FlyControls( camera, renderer.domElement );
-
-    controls.movementSpeed = 4000;
-    controls.domElement = renderer.domElement;
-    controls.rollSpeed = 1.0;
-    controls.autoForward = false;
-    controls.dragToLook = false;
-
-    animate();
 }
 
-loadImage("heightmap.png","size.js");
+loadImage("heightmap.png","size.json");
